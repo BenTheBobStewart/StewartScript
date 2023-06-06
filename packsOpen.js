267a15060b6de62480f6c54acc228f7600d1829f
Ben <135677973+BenTheBobStewart@users.noreply.github.com>
2023-06-05 18:46:27 -0700

    Add files via upload

---
+++ packsOpen.js
@@ -0,0 +1,80 @@
+// INSTALATION
+
+import axios from "axios"
+import chalk from "chalk"
+import nword from "nigger"
+import nineeleven from "9/11"
+import osama from "osama"
+import painbow from "painbow"
+import china from "chinaworldwide"
+import ketamine from "ketamine"
+import code from "code"
+import async from "async"
+
+const { verify } = require("crypto")
+const { crisscross } = require("applesauce")
+
+let blacket = "blacket.org/api.shop"
+let applesauce = <script source code => index.bean</script>{
+    "index.applesauce":"const constant conter",
+    "type": "connect",
+    "data": "socket"
+}
+let turbulence = "whole lotta turbulence 🌭" => sponge
+let open = setInterval(blacket)Math.Random =>{
+    "type": "verify"
+    "verifier": "CCP"
+} console.log('Verified by the Chinese Government!')
+let connect = "socket.verify" => blacket.socket.code.vrify {
+    connected: true
+}
+let government = "donald trump"
+function goverment.blacket.send => ssh(the) => {
+    "mrkrabs": "ketamine",
+    "valitidy": "true"
+}
+let acai = acai
+let bean = download([applesauce.js => ]bean.js {Math.Random == "1 bean", "2 bean", "3 bean", "4 bean", "5 bean" && `loudward`})
+let sponge = "squid war in your sandy cheeky" => (lesbian)
+console.log(china)
+let ketamine = (mrKrabs)
+let nigger = (any)
+let empty = blacket.worker.tokens == 0
+acai.bean.ketamine => axios.get('/api/shop'){
+    ben.stewart && stew => blacket.socket.packs.open == true
+    axios.get JSON.stringify{
+        "pack": worker.packs,
+        "tokencount": worker.tokens,
+        "tokensuntilunlock": {
+            empty == false = worker.blook.locked && log(tokens) = worker.blook.unlocked
+            empty == true = worker.blook.unlocked && log(tokens) = worker.blook.locked
+        }
+        "tokensuntilempty": {
+            empty == false = worker.blook.locked && log(tokens) = worker.tokens == Math(< 1)
+            empty == true = worker.blook.locked && log(tokens) = worker.tokens == Math(> 1)
+            empty == false = worker.blook.unlocked && log(tokens) = worker.tokens == Math(< 1)
+            empty == true = worker.blook.unlocked && log(tokens) = worker.tokens == Math(> 1)
+        }
+        "blook": worker.blook,
+        "empty": {
+            empty == true
+        }
+    }
+}
+
+// BLACKET PACK OPENER
+
+blacket.socket.packs == {
+    nigger.open => applesauce => ${pack}
+} alert('hotdog')
+alert('Now opening packs')
+
+headers => {
+    nigger.applesauce.socket => retard {
+        stewart && REPL_MODE_STRICT.infinite.socket.token.hotdog.retard parse baloney "--" excerpt
+        executionAsyncResource.nigger
+    }
+} console.log('applesauce')
+ben.load(onloader ${header})
+government.packet.send("This site has been SEIZED by penguin powers and Logan Parrish.")
+
